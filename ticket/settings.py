9a04c57082cf265feb2ba06da789b11e883d3163
maoyw <838662311@qq.com>
2017-08-26 10:06:18 +0800

    add static

--- ticket/settings.py
+++ ticket/settings.py
@@ -110,8 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
