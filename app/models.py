e0dc7c3cbeb01ba9ee934fc179ff1f3208d087a2
xxl徐新龙 <xinlongxu@ctrip.com>
2017-08-26 16:29:30 +0800

    change struct

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from ticket.db import TimestampedModel
+from distribution.db import TimestampedModel
 
 # Create your models here.
 
--- app/views.py
+++ app/views.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 
 
 def index(request):
-    return render_to_response("app/index.html")
+    # return render_to_response("app/index.html")
+    return render(request, "app/index.html")
 
 
 def create_ticket(request):
-    return render_to_response("app/create_ticket.html")
+    # return render_to_response("app/create_ticket.html")
+    return render(request, "app/create_ticket.html")
